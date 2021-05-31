f102368ff217d81723648dbed4e7625c0b663a3b
Luligabi1 <luligabi111@gmail.com>
2021-05-31 20:46:30 -0300

    Added Brewing Stand message's translation entry.

--- src/main/java/me/luligabi/fuelinfo/mixin/BrewingStandScreenMixin.java
+++ src/main/java/me/luligabi/fuelinfo/mixin/BrewingStandScreenMixin.java
@@ -3,7 +3,9 @@ package me.luligabi.fuelinfo.mixin;
 import net.minecraft.client.gui.screen.ingame.BrewingStandScreen;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.screen.BrewingStandScreenHandler;
-import net.minecraft.text.LiteralText;
+import net.minecraft.text.Style;
+import net.minecraft.text.TranslatableText;
+import net.minecraft.util.Formatting;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -20,7 +22,8 @@ public class BrewingStandScreenMixin {
         BrewingStandScreenHandler bssh = bss.getScreenHandler();
         int fuelCount = bssh.getFuel() + (bssh.getSlot(4).getStack().getCount() * 20);
 
-        bss.renderTooltip(matrices, new LiteralText("" + fuelCount*3), mouseX, mouseY);
+        bss.renderTooltip(matrices, new TranslatableText("message.fuelinfo.brewing_stand",
+                fuelCount*3).setStyle(Style.EMPTY).formatted(Formatting.GRAY), mouseX, mouseY);
 
         callbackInfo.cancel();
     }
