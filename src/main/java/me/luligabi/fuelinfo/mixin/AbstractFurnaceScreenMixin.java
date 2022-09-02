47d20b11acb207016154cc9f5d74c1ffe1e2111a
Luligabi1 <33578169+Luligabi1@users.noreply.github.com>
2022-09-01 22:43:03 -0300

    1.0.1 backport

--- src/main/java/me/luligabi/fuelinfo/mixin/AbstractFurnaceScreenMixin.java
+++ src/main/java/me/luligabi/fuelinfo/mixin/AbstractFurnaceScreenMixin.java
@@ -7,8 +7,6 @@ import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.item.Item;
 import net.minecraft.screen.AbstractFurnaceScreenHandler;
-import net.minecraft.screen.ScreenHandler;
-import net.minecraft.text.Style;
 import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
 import net.minecraft.util.Formatting;
@@ -20,18 +18,16 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import java.util.Map;
 
 @Mixin(AbstractFurnaceScreen.class)
-public abstract class AbstractFurnaceScreenMixin extends HandledScreen {
+public abstract class AbstractFurnaceScreenMixin extends HandledScreen<AbstractFurnaceScreenHandler> {
 
-    public AbstractFurnaceScreenMixin(ScreenHandler handler, PlayerInventory inventory, Text title) {
+    public AbstractFurnaceScreenMixin(AbstractFurnaceScreenHandler handler, PlayerInventory inventory, Text title) {
         super(handler, inventory, title);
     }
 
-    @Inject(method = "render",
-            at = @At("RETURN"),
-            cancellable = true)
+    @Inject(method = "render", at = @At("TAIL"))
     public void render(MatrixStack matrices, int mouseX, int mouseY, float delta, CallbackInfo callbackInfo) {
-        AbstractFurnaceScreen afs = ((AbstractFurnaceScreen) (Object) this);
-        AbstractFurnaceScreenHandler afsh = (((AbstractFurnaceScreenHandler) afs.getScreenHandler()));
+        AbstractFurnaceScreen<AbstractFurnaceScreenHandler> afs = ((AbstractFurnaceScreen<AbstractFurnaceScreenHandler>) (Object) this);
+        AbstractFurnaceScreenHandler afsh = afs.getScreenHandler();
 
         int inventoryX = this.x;
         int inventoryY = this.y;
@@ -39,27 +35,26 @@ public abstract class AbstractFurnaceScreenMixin extends HandledScreen {
         // Check if player's mouse is hovering the "flame" icon between entry and fuel slots.
         if((mouseX >= inventoryX+56 && mouseX <= inventoryX+72) && (mouseY >= inventoryY+34 && mouseY <= inventoryY+50)) {
 
-        // Calculates how many fuel ticks there are within already consumed items.
-        int consumedFuelTicks = ((AbstractFurnaceScreenHandlerAccessor) afsh).getPropertyDelegate().get(0);
+            // Calculates how many fuel ticks there are within already consumed items.
+            int consumedFuelTicks = ((AbstractFurnaceScreenHandlerAccessor) afsh).getPropertyDelegate().get(0);
 
-        // Adds +1 item to the count if the furnace is burning still (avoids slight miscalculation)
-        if(afsh.isBurning()) {
-            consumedFuelTicks += 200;
+            // Adds +1 item to the count if the furnace is burning still (avoids slight miscalculation)
+            if(afsh.isBurning()) {
+                consumedFuelTicks += 200;
+            }
+
+            // Calculates how many burning ticks there are within items in the fuel slot, but that haven't been consumed yet.
+            Map<Item, Integer> fuelMap = AbstractFurnaceBlockEntity.createFuelTimeMap();
+            int toBeConsumedFuelTicks = 0;
+
+            if(fuelMap.containsKey(afsh.getSlot(1).getStack().getItem())) {
+                toBeConsumedFuelTicks += fuelMap.get(afsh.getSlot(1).getStack().getItem()) * afsh.getSlot(1).getStack().getCount();
+            }
+
+            // Fuel Ticks are divided by 200 to get how many items can be smelt.
+            afs.renderTooltip(matrices, new TranslatableText("message.fuelinfo.furnace",
+                            (consumedFuelTicks + toBeConsumedFuelTicks)/200),
+                    mouseX, mouseY);
         }
-
-        // Calculates how many burning ticks there are within items in the fuel slot, but that haven't been consumed yet.
-        Map<Item, Integer> fuelMap = AbstractFurnaceBlockEntity.createFuelTimeMap();
-        int toBeConsumedFuelTicks = 0;
-
-        if(fuelMap.containsKey(afsh.getSlot(1).getStack().getItem())) {
-            toBeConsumedFuelTicks += fuelMap.get(afsh.getSlot(1).getStack().getItem()) * afsh.getSlot(1).getStack().getCount();
-        }
-
-        // Fuel Ticks are divided by 200 to get how many items can be smelt.
-        afs.renderTooltip(matrices, new TranslatableText("message.fuelinfo.furnace",
-          (consumedFuelTicks + toBeConsumedFuelTicks)/200).setStyle(Style.EMPTY).formatted(Formatting.GRAY)
-                , mouseX, mouseY);
-        }
-        callbackInfo.cancel();
     }
 }
