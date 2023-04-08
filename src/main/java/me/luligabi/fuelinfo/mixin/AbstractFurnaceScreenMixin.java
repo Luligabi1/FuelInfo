3764cbe10c27bb6f1de8a341b3818124ea4791cc
Luligabi1 <33578169+Luligabi1@users.noreply.github.com>
2023-04-07 22:00:29 -0300

    Update to 1.19.4, fix glitch on furnace fuel calculation

--- src/main/java/me/luligabi/fuelinfo/mixin/AbstractFurnaceScreenMixin.java
+++ src/main/java/me/luligabi/fuelinfo/mixin/AbstractFurnaceScreenMixin.java
@@ -6,9 +6,9 @@ import net.minecraft.client.gui.screen.ingame.HandledScreen;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.screen.AbstractFurnaceScreenHandler;
 import net.minecraft.text.Text;
-import net.minecraft.util.Formatting;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -34,26 +34,26 @@ public abstract class AbstractFurnaceScreenMixin extends HandledScreen<AbstractF
         // Check if player's mouse is hovering the "flame" icon between entry and fuel slots.
         if((mouseX >= inventoryX+56 && mouseX <= inventoryX+72) && (mouseY >= inventoryY+34 && mouseY <= inventoryY+50)) {
 
-        // Calculates how many fuel ticks there are within already consumed items.
-        int consumedFuelTicks = ((AbstractFurnaceScreenHandlerAccessor) afsh).getPropertyDelegate().get(0);
+            // Get how many fuel ticks there are within already consumed items
+            int consumedFuelTicks = ((AbstractFurnaceScreenHandlerAccessor) afsh).getPropertyDelegate().get(0);
 
-        // Adds +1 item to the count if the furnace is burning still (avoids slight miscalculation)
-        if(afsh.isBurning()) {
-            consumedFuelTicks += 200;
-        }
 
-        // Calculates how many burning ticks there are within items in the fuel slot, but that haven't been consumed yet.
-        Map<Item, Integer> fuelMap = AbstractFurnaceBlockEntity.createFuelTimeMap();
-        int toBeConsumedFuelTicks = 0;
+            // Adds +1 item to the count if the furnace is burning still (avoids slight miscalculation)
+            if(afsh.isBurning()) consumedFuelTicks += 100;
 
-        if(fuelMap.containsKey(afsh.getSlot(1).getStack().getItem())) {
-            toBeConsumedFuelTicks += fuelMap.get(afsh.getSlot(1).getStack().getItem()) * afsh.getSlot(1).getStack().getCount();
-        }
+            // Get how many burning ticks there are within items in the fuel slot, but that haven't been consumed yet.
+            Map<Item, Integer> fuelMap = AbstractFurnaceBlockEntity.createFuelTimeMap();
+            int toBeConsumedFuelTicks = 0;
 
-        // Fuel Ticks are divided by 200 to get how many items can be smelt.
-        afs.renderTooltip(matrices, Text.translatable("message.fuelinfo.furnace",
-          (consumedFuelTicks + toBeConsumedFuelTicks)/200),
-                mouseX, mouseY);
+            ItemStack slotStack = afsh.getSlot(1).getStack();
+            if(fuelMap.containsKey(slotStack.getItem())) {
+                toBeConsumedFuelTicks += fuelMap.get(slotStack.getItem()) * slotStack.getCount();
+            }
+
+            // consumed ticks are divided by 100, while unconsumed are by 200. don't ask me why it works like that lol
+            afs.renderTooltip(matrices, Text.translatable("message.fuelinfo.furnace",
+            (consumedFuelTicks/100) + (toBeConsumedFuelTicks/200)),
+                    mouseX, mouseY);
         }
     }
 }
