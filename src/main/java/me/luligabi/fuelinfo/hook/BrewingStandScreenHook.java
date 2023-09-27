d281ba82c9dd98c5721d2666b358902857a1e340
Luligabi1 <33578169+Luligabi1@users.noreply.github.com>
2023-09-27 15:48:07 -0300

    Add shift-to-set feature for Brewing Stands (part of #2)

--- src/main/java/me/luligabi/fuelinfo/hook/BrewingStandScreenHook.java
+++ src/main/java/me/luligabi/fuelinfo/hook/BrewingStandScreenHook.java
@@ -4,6 +4,7 @@ import me.luligabi.fuelinfo.mixin.HandledScreenAccessor;
 import me.luligabi.fuelinfo.util.TimerUtil;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.ingame.BrewingStandScreen;
 import net.minecraft.screen.BrewingStandScreenHandler;
 import net.minecraft.text.Text;
@@ -18,14 +19,25 @@ public class BrewingStandScreenHook {
         int y = ((HandledScreenAccessor) screen).getY();
 
         if((mouseX >= x+58 && mouseX <= x+78) && (mouseY >= y+42 && mouseY <= y+48)) {
-            int fuel = (screenHandler.getFuel() * 3) + (screenHandler.getSlot(4).getStack().getCount() * 20) * 3;
+            int potionCount = (screenHandler.getFuel() * 3) + (screenHandler.getSlot(4).getStack().getCount() * 20) * 3;
 
-            if(fuel > 0) {
-                Text fuelText = Text.translatable(
-                    "message.fuelinfo.brewing_stand",
-                    fuel
-                );
-                ctx.drawTooltip(MinecraftClient.getInstance().textRenderer, fuelText, mouseX, mouseY);
+            if(potionCount > 0) {
+                Text countText;
+
+                if(Screen.hasShiftDown()) {
+                    countText = Text.translatable(
+                        "message.fuelinfo.brewing_stand.set",
+                        potionCount / 3
+                    );
+                } else {
+                    countText = Text.translatable(
+                        "message.fuelinfo.brewing_stand",
+                        potionCount
+                    );
+                }
+
+
+                ctx.drawTooltip(MinecraftClient.getInstance().textRenderer, countText, mouseX, mouseY);
             }
         }
 
