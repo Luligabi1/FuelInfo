b47877b913be1ddb5dcf84f2c28e1a144ea61b88
Luiz Feikes <33578169+Luligabi1@users.noreply.github.com>
2024-05-11 16:01:39 -0300

    Add custom info logic (WIP)

--- common/src/main/java/me/luligabi/fuelinfo/hook/AbstractFurnaceMenuHook.java
+++ common/src/main/java/me/luligabi/fuelinfo/hook/AbstractFurnaceMenuHook.java
@@ -16,10 +16,14 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.RecipeType;
 import net.minecraft.world.level.block.entity.AbstractFurnaceBlockEntity;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
-public class AbstractFurnaceMenuHook {
+public class AbstractFurnaceMenuHook { // TODO add config values
 
+
+    private static final List<Component> COMPONENTS = new ArrayList<>();
 
     public static void render(AbstractFurnaceScreen<AbstractFurnaceMenu> screen, GuiGraphics gui, int mouseX, int mouseY) {
         AbstractFurnaceMenu menu = screen.getMenu();
@@ -27,77 +31,96 @@ public class AbstractFurnaceMenuHook {
         int x = ((AbstractContainerScreenAccessor) screen).getX();
         int y = ((AbstractContainerScreenAccessor) screen).getY();
 
-        // Check if player's mouse is hovering the "flame" icon between entry and fuel slots.
         if((mouseX >= x + 56 && mouseX <= x + 72) && (mouseY >= y + 35 && mouseY <= y + 50)) {
-            // Get how many fuel ticks there are within already consumed items
-            int consumedFuelTicks = data.get(0);
-
-            // Add +1 item to the count if the furnace is burning still,
-            // accounting the item that is currently being smelted
-            if(menu.isLit()) {
-                consumedFuelTicks += isSpecialFurnace(menu) ? 100 : 200;
-            }
-
-            // Get how many burning ticks there are within items in the fuel slot, but that haven't been consumed yet.
-            Map<Item, Integer> fuelMap = AbstractFurnaceBlockEntity.getFuel();
-            int toBeConsumedFuelTicks = 0;
-
-            ItemStack fuelStack = menu.getSlot(1).getItem();
-            if(fuelMap.containsKey(fuelStack.getItem())) {
-                toBeConsumedFuelTicks += fuelMap.get(fuelStack.getItem()) * fuelStack.getCount();
-            }
-
-            // combine fuel ticks
-            if(isSpecialFurnace(menu)) {
-                consumedFuelTicks /= 100;
-            } else {
-                consumedFuelTicks /= 200;
-            }
-            int i = (consumedFuelTicks) + (toBeConsumedFuelTicks / 200);
-            if(i > 0) {
-                // Fuel Info Logic
-                Component fuelText;
-                if(!Screen.hasShiftDown()) {
-                    int stacks = i / 64;
-                    int items = i % 64;
-
-                    if (stacks > 0) {
-                        if (items > 0) {
-                            fuelText = Component.translatable("message.fuelinfo.furnace.both", stacks, items);
-                        } else {
-                            fuelText = Component.translatable("message.fuelinfo.furnace.stacks", stacks);
-                        }
-                    } else {
-                        fuelText = Component.translatable("message.fuelinfo.furnace.items", items);
-                    }
-                } else { // Shift-action: Don't show stacks
-                    fuelText = Component.translatable("message.fuelinfo.furnace.items", i);
-                }
-                gui.renderTooltip(Minecraft.getInstance().font, fuelText, mouseX, mouseY);
+            addFuelData(menu, data, true);
+            addTimer(menu, data, false);
+            if(!COMPONENTS.isEmpty()) {
+                gui.renderComponentTooltip(Minecraft.getInstance().font, COMPONENTS, mouseX, mouseY);
+                COMPONENTS.clear();
             }
         }
-
         if((mouseX >= x + 80 && mouseX <= x + 102) && (mouseY >= y + 35 && mouseY <= y + 50)) {
-            Component timeText;
-            ItemStack inputStack = menu.getSlot(0).getItem();
-
-            if(!inputStack.isEmpty()) {
-                float tickrate = ((AbstractFurnaceMenuAccessor) menu).getLevel().tickRateManager().tickrate();
-
-                float currentStackTime = (data.get(3) - data.get(2));
-                float remainingStacksTime = (data.get(3) * (inputStack.getCount() - 1));
-                int time = Math.round((currentStackTime + remainingStacksTime) / tickrate);
-
-                boolean isIndividualTime = Screen.hasShiftDown();
-                Tuple<String, String> timePair = TimerUtil.getTime(isIndividualTime ? Math.round(currentStackTime / tickrate) : time);
-
-                timeText = Component.translatable(
-                        "message.fuelinfo.timer" + (isIndividualTime ? ".current" : ""),
-                        timePair.getA(), timePair.getB()
-                );
-                gui.renderTooltip(Minecraft.getInstance().font, timeText, mouseX, mouseY);
+            addFuelData(menu, data, false);
+            addTimer(menu, data, true);
+            if(!COMPONENTS.isEmpty()) {
+                gui.renderComponentTooltip(Minecraft.getInstance().font, COMPONENTS, mouseX, mouseY);
+                COMPONENTS.clear();
             }
         }
+    }
+
+    private static void addFuelData(AbstractFurnaceMenu menu, ContainerData data, boolean canAdd) {
+        if(!canAdd) return;
+
+        // Get how many fuel ticks there are within already consumed items
+        int consumedFuelTicks = data.get(0);
+
+        // Add +1 item to the count if the furnace is burning still,
+        // accounting the item that is currently being smelted
+        if(menu.isLit()) {
+            consumedFuelTicks += isSpecialFurnace(menu) ? 100 : 200;
+        }
+
+        // Get how many burning ticks there are within items in the fuel slot, but that haven't been consumed yet
+        Map<Item, Integer> fuelMap = AbstractFurnaceBlockEntity.getFuel();
+        int toBeConsumedFuelTicks = 0;
+
+        ItemStack fuelStack = menu.getSlot(1).getItem();
+        if(fuelMap.containsKey(fuelStack.getItem())) {
+            toBeConsumedFuelTicks += fuelMap.get(fuelStack.getItem()) * fuelStack.getCount();
+        }
+
+        // Account for the fact special furnaces (i.e. Blast Furnace
+        // and Smoker on vanilla) smelts stuff twice as fast
+        if(isSpecialFurnace(menu)) {
+            consumedFuelTicks /= 100;
+        } else {
+            consumedFuelTicks /= 200;
+        }
+        // Combine fuel ticks
+        int i = (consumedFuelTicks) + (toBeConsumedFuelTicks / 200);
+        if(i <= 0) return;
+        Component fuelText;
+        if(!Screen.hasShiftDown()) {
+            int stacks = i / 64;
+            int items = i % 64;
+
+            if(stacks > 0) {
+                if(items > 0) {
+                    fuelText = Component.translatable("message.fuelinfo.furnace.both", stacks, items);
+                } else {
+                    fuelText = Component.translatable("message.fuelinfo.furnace.stacks", stacks);
+                }
+            } else {
+                fuelText = Component.translatable("message.fuelinfo.furnace.items", items);
+            }
+        } else { // Shift-action: Don't show stacks
+            fuelText = Component.translatable("message.fuelinfo.furnace.items", i);
+        }
+        COMPONENTS.add(fuelText);
+    }
+
+    private static void addTimer(AbstractFurnaceMenu menu, ContainerData data, boolean canAdd) {
+        if(!canAdd) return;
+
+        Component timeText;
+        ItemStack inputStack = menu.getSlot(0).getItem();
+
+        if(inputStack.isEmpty()) return;
+        float tickrate = ((AbstractFurnaceMenuAccessor) menu).getLevel().tickRateManager().tickrate();
+
+        float currentStackTime = (data.get(3) - data.get(2));
+        float remainingStacksTime = (data.get(3) * (inputStack.getCount() - 1));
+        int time = Math.round((currentStackTime + remainingStacksTime) / tickrate);
+
+        boolean isIndividualTime = Screen.hasShiftDown();
+        Tuple<String, String> timePair = TimerUtil.getTime(isIndividualTime ? Math.round(currentStackTime / tickrate) : time);
+
+        timeText = Component.translatable(
+            "message.fuelinfo.timer" + (isIndividualTime ? ".current" : ""),
+            timePair.getA(), timePair.getB()
+        );
+        COMPONENTS.add(timeText);
     }
 
     // TODO: Investigate if there's better way to determine this on Neoforge
--- common/src/main/java/me/luligabi/fuelinfo/hook/BrewingStandScreenHook.java
+++ common/src/main/java/me/luligabi/fuelinfo/hook/BrewingStandScreenHook.java
@@ -10,9 +10,14 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.world.inventory.BrewingStandMenu;
 import net.minecraft.world.level.Level;
 
+import java.util.ArrayList;
+import java.util.List;
 
-public class BrewingStandScreenHook {
 
+public class BrewingStandScreenHook { // TODO add config values
+
+
+    private static final List<Component> COMPONENTS = new ArrayList<>();
 
     public static void render(BrewingStandScreen screen, GuiGraphics gui, int mouseX, int mouseY, Level level) {
         BrewingStandMenu menu = screen.getMenu();
@@ -20,36 +25,54 @@ public class BrewingStandScreenHook {
         int y = ((AbstractContainerScreenAccessor) screen).getY();
 
         if((mouseX >= x + 58 && mouseX <= x + 78) && (mouseY >= y + 42 && mouseY <= y + 48)) {
-            int potionCount = (menu.getFuel() * 3) + (menu.getSlot(4).getItem().getCount() * 20) * 3;
-
-            if(potionCount > 0) {
-                Component countText;
-
-                if(Screen.hasShiftDown()) {
-                    countText = Component.translatable(
-                        "message.fuelinfo.brewing_stand.set",
-                        potionCount / 3
-                    );
-                } else {
-                    countText = Component.translatable(
-                        "message.fuelinfo.brewing_stand",
-                        potionCount
-                    );
-                }
-                gui.renderTooltip(Minecraft.getInstance().font, countText, mouseX, mouseY);
+            addFuelData(menu, true);
+            addTimer(menu, level, false);
+            if(!COMPONENTS.isEmpty()) {
+                gui.renderComponentTooltip(Minecraft.getInstance().font, COMPONENTS, mouseX, mouseY);
+                COMPONENTS.clear();
             }
         }
 
         if((mouseX >= x + 98 && mouseX <= x + 104) && (mouseY >= y + 17 && mouseY <= y + 42)) {
-            int time = Math.round(menu.getBrewingTicks() / level.tickRateManager().tickrate());
-
-            if(time > 0) {
-                Component timeText = Component.translatable(
-                    "message.fuelinfo.timer",
-                    "00", TimerUtil.format(time)
-                );
-                gui.renderTooltip(Minecraft.getInstance().font, timeText, mouseX, mouseY);
+            addFuelData(menu, false);
+            addTimer(menu, level, true);
+            if(!COMPONENTS.isEmpty()) {
+                gui.renderComponentTooltip(Minecraft.getInstance().font, COMPONENTS, mouseX, mouseY);
+                COMPONENTS.clear();
             }
         }
+    }
+
+    private static void addFuelData(BrewingStandMenu menu, boolean canAdd) {
+        if(!canAdd) return;
+        int potionCount = (menu.getFuel() * 3) + (menu.getSlot(4).getItem().getCount() * 20) * 3;
+
+        if(potionCount <= 0) return;
+        Component countText;
+
+        if(Screen.hasShiftDown()) {
+            countText = Component.translatable(
+                "message.fuelinfo.brewing_stand.set",
+                potionCount / 3
+            );
+        } else {
+            countText = Component.translatable(
+                "message.fuelinfo.brewing_stand",
+                potionCount
+            );
+        }
+        COMPONENTS.add(countText);
+    }
+
+    private static void addTimer(BrewingStandMenu menu, Level level, boolean canAdd) {
+        if(!canAdd) return;
+        int time = Math.round(menu.getBrewingTicks() / level.tickRateManager().tickrate());
+
+        if(time <= 0) return;
+        Component timeText = Component.translatable(
+            "message.fuelinfo.timer",
+            "00", TimerUtil.format(time)
+        );
+        COMPONENTS.add(timeText);
     }
 }
